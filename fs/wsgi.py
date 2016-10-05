ff7af55f72eac51d7f40f3dcac52fbf42eb6bec7
Richard Neumann <r.neumann@homeinfo.de>
2016-10-05 10:08:48 +0200

    Implementing...

--- fs/wsgi.py
+++ fs/wsgi.py
@@ -6,10 +6,19 @@ from his.api.locale import Language
 from his.api.errors import HISMessage
 from his.api.handlers import AuthorizedService
 
-from .orm import Inode
+from .orm import NotAFile, FileNotFound, Inode
 
 
-class FileNotAvailable(HISMessage):
+class FileSystemError(HISMessage):
+    """Indicates that the respective file is not available"""
+
+    STATUS = 500
+    LOOCALE = {
+        Language.DE_DE: 'Dateisystemfehler.',
+        Language.EN_US: 'File system error.'}
+
+
+class FileNotAvailable(FileSystemError):
     """Indicates that the respective file is not available"""
 
     STATUS = 500
@@ -18,43 +27,47 @@ class FileNotAvailable(HISMessage):
         Language.EN_US: 'File not available.'}
 
 
+class FileNotFound(FileSystemError):
+    """Indicates that the respective file is not available"""
+
+    STATUS = 404
+    LOOCALE = {
+        Language.DE_DE: 'Datei nicht gefunden.',
+        Language.EN_US: 'File not found.'}
+
+
 class FileManager(AuthorizedService):
     """Service that manages files"""
 
     def get(self):
-        """Retrieves information about a file"""
+        """Retrieves (a) file(s)"""
         if self.resource is None:
-            # Retrieves all files
-            # TODO: implement
-            pass
+            return JSON(Inode.fsdict(cls, owner=self.account))
         else:
             try:
-                file_id = int(self.resource)
-            except ValueError:
-                try:
-                    inode = Inode.by_path(self.resource, account=self.account)
-                except (NoSuchNode, NotADirectory):
-                    raise
-                else:
-                    try:
-                        return inode.data
-                    except NotAFile:
-                        # TODO: handle
-                        pass
-                    except FileNotFound:
-                        raise FileNotAvailable() from None
+                inode = Inode.by_path(self.resource, account=self.account)
+            except (NoSuchNode, NotADirectory):
+                raise FileNotFound() from None
             else:
                 try:
-                    inode = Inode.by_id(self.account, file_id)
-                except DoesNotExist:
-                    # TODO: implement
-                    raise NoSuchInode()
-                else:
-                    try:
-                        return inode.data
-                    except NotAFile:
-                        # TODO: handle
-                        pass
-                    except FileNotFound:
-                        # TODO: handle
-                        pass
+                    return inode.data
+                except NotAFile:
+                    # TODO: handle directory
+                    pass
+                except FileNotFound:
+                    raise FileNotAvailable() from None
+
+    def post(self):
+        """Adds new files"""
+        # TODO: implement
+        pass
+
+    def put(self):
+        """Updates an existing file"""
+        # TODO: implement
+        pass
+
+    def delete(self):
+        """Deletes a file"""
+        # TODO: implement
+        pass
