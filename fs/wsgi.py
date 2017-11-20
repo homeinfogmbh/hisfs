945192c4b841290fd3cdda26644273edf6547a2a
Richard Neumann <r.neumann@homeinfo.de>
2017-11-20 16:27:26 +0100

    Refactored FS.

--- fs/wsgi.py
+++ fs/wsgi.py
@@ -9,7 +9,7 @@ from wsgilib import OK, JSON, Binary
 from filedb import FileError
 
 from his.api.messages import NotAnInteger
-from his.api.handlers import AuthorizedService
+from his.api.handlers import service, AuthorizedService
 
 from .messages import NotADirectory, NoSuchNode, DeletionError, \
     NoFileNameSpecified, InvalidFileName, FileExists, FileCreated, \
@@ -21,13 +21,9 @@ from .orm import Inode
 __all__ = ['FS']
 
 
+@service('fs')
 class FS(AuthorizedService):
     """Service that manages files."""
-
-    NODE = 'fs'
-    NAME = 'FileSystem'
-    DESCRIPTION = 'Dateisystem'
-    PROMOTE = False
 
     @property
     def sha256sum(self):
@@ -56,108 +52,104 @@ class FS(AuthorizedService):
         """Returns the recursive flag."""
         return
 
+    @property
+    def inode(self):
+        """Returns the respective INode."""
+        return Inode.by_path(
+            self.resource, owner=self.account, group=self.group)
+
     def get(self):
         """Retrieves (a) file(s)."""
         if self.resource is None:
             root = Inode.root_for(owner=self.account, group=self.customer)
             return JSON(root.dict_for(self.account))
-        else:
-            try:
-                inode = Inode.by_path(
-                    self.resource,
-                    owner=self.account,
-                    group=self.group)
-            except DoesNotExist:
-                raise NoSuchNode() from None
-            else:
-                if inode.readable_by(self.account):
-                    with suppress(KeyError):
-                        # Access self.sha256sum first to trigger a possible
-                        # KeyError before the resource-hungry inode.sha256sum
-                        # is invoked.
-                        if self.sha256sum == inode.sha256sum:
-                            return FileUnchanged()
 
-                    if self.query.get('sha256sum', False):
-                        return OK(inode.sha256sum)
+        try:
+            inode = Inode.by_path(
+                self.resource, owner=self.account, group=self.group)
+        except DoesNotExist:
+            raise NoSuchNode() from None
 
-                    return Binary(inode.data)
+        if inode.readable_by(self.account):
+            with suppress(KeyError):
+                # Access self.sha256sum first to trigger a possible
+                # KeyError before the resource-hungry inode.sha256sum
+                # is invoked.
+                if self.sha256sum == inode.sha256sum:
+                    return FileUnchanged()
 
-                raise NotReadable() from None
+            if self.query.get('sha256sum', False):
+                return OK(inode.sha256sum)
+
+            return Binary(inode.data)
+
+        raise NotReadable() from None
 
     def post(self):
         """Adds new files."""
         if self.resource is None:
             raise NoFileNameSpecified()
-        else:
+
+        try:
+            Inode.by_path(self.resource, owner=self.account, group=self.group)
+        except DoesNotExist:
+            parent = dirname(self.resource)
+
             try:
-                Inode.by_path(
-                    self.resource,
-                    owner=self.account,
-                    group=self.group)
+                parent = Inode.by_path(
+                    parent, owner=self.account, group=self.group)
             except DoesNotExist:
-                parent = dirname(self.resource)
+                raise ParentDirDoesNotExist() from None
 
-                try:
-                    parent = Inode.by_path(
-                        parent,
-                        owner=self.account,
-                        group=self.group)
-                except DoesNotExist:
-                    raise ParentDirDoesNotExist() from None
-                else:
-                    if parent.isdir:
-                        if parent.writable_by(self.account):
-                            inode = Inode()
+            if parent.isdir:
+                if parent.writable_by(self.account):
+                    inode = Inode()
 
-                            try:
-                                inode.name = basename(self.resource)
-                            except ValueError:
-                                raise InvalidFileName()
-                            else:
-                                inode.owner = self.account
-                                inode.group = self.customer
-                                inode.parent = parent
+                    try:
+                        inode.name = basename(self.resource)
+                    except ValueError:
+                        raise InvalidFileName()
+                    else:
+                        inode.owner = self.account
+                        inode.group = self.customer
+                        inode.parent = parent
 
-                                if self.data.bytes:
-                                    inode.data = self.data.bytes
+                        if self.data.bytes:
+                            inode.data = self.data.bytes
 
-                                inode.mode = self.mode
-                                inode.save()
-                                return FileCreated()
+                        inode.mode = self.mode
+                        inode.save()
+                        return FileCreated()
 
-                        raise NotWritable() from None
+                raise NotWritable() from None
 
-                    raise NotADirectory() from None
-            else:
-                raise FileExists() from None
+            raise NotADirectory() from None
+
+        raise FileExists() from None
 
     def delete(self):
         """Deletes a file."""
         if self.resource is None:
             raise NoFileNameSpecified()
-        else:
+
+        try:
+            inode = Inode.by_path(self.resource, owner=self.account, group=self.group)
+        except DoesNotExist:
+            raise NoSuchNode() from None
+
+        if inode.parent is None:
+            raise RootDeletionError() from None
+
+        if inode.parent.writable_by(self.account):
             try:
-                inode = Inode.by_path(
-                    self.resource,
-                    owner=self.account,
-                    group=self.group)
-            except DoesNotExist:
-                raise NoSuchNode() from None
-            else:
-                if inode.parent is None:
-                    raise RootDeletionError() from None
+                inode.remove(recursive=self.query.get(
+                    'recursive', False))
+            except FileError:
+                raise DeletionError() from None
 
-                if inode.parent.writable_by(self.account):
-                    try:
-                        inode.remove(recursive=self.query.get(
-                            'recursive', False))
-                    except FileError:
-                        raise DeletionError() from None
-                    else:
-                        return FileDeleted()
+            return FileDeleted()
 
-                raise NotWritable() from None
+        raise NotWritable() from None
 
     def options(self):
         """Returns options information."""
