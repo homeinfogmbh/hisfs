8aa2043399be8adb13a7db26cdcdb0c2359ab82b
Richard Neumann <r.neumann@homeinfo.de>
2017-11-21 11:51:03 +0100

    Refactored setup.py.

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ from distutils.core import setup
 setup(
     name='his.fs',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info at homeinfo dot de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r dot neumann at homeinfo period de>',
     requires=['his'],
     package_dir={'his.mods': ''},
     packages=['his.mods.fs'],
-    data_files=[('/etc/his.d/locale', ['files/etc/his.d/locale/fs.ini'])],
+    data_files=[('/etc/his.d/locale', ['files/fs.ini'])],
     description='HOMEINFO Integrated Services File System')
