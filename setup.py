a85ea777145511bdd76a175355f5fd07dd374231
Richard Neumann <r.neumann@homeinfo.de>
2018-01-25 14:45:55 +0100

    Renamed package.

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from distutils.core import setup
 
 
 setup(
-    name='his.fs',
+    name='hisfs',
     version='latest',
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo period de>',
     requires=['his'],
-    package_dir={'his.mods': ''},
-    packages=['his.mods.fs'],
+    packages=['hisfs'],
     data_files=[('/etc/his.d/locale', ['files/fs.ini'])],
     description='HOMEINFO Integrated Services File System')
