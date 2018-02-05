7d7c7c1a3a875779d945deb14d68c50658d03b17
Richard Neumann <r.neumann@homeinfo.de>
2018-02-05 15:28:21 +0100

    Added script and systemd service.

--- setup.py
+++ setup.py
@@ -12,5 +12,8 @@ setup(
     maintainer_email='<r dot neumann at homeinfo period de>',
     requires=['his'],
     packages=['hisfs'],
-    data_files=[('/etc/his.d/locale', ['files/fs.ini'])],
+    scripts=['files/hisfsd'],
+    data_files=[
+        ('/usr/lib/systemd/system', ['files/hisfs.service']),
+        ('/etc/his.d/locale', ['files/fs.ini'])],
     description='HOMEINFO Integrated Services File System')
