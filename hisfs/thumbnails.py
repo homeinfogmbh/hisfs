745d02c5803e40e010bacfcd1f5338d0ca47195a
Richard Neumann <r.neumann@homeinfo.de>
2018-12-06 12:45:11 +0100

    Fix.

--- hisfs/thumbnails.py
+++ hisfs/thumbnails.py
@@ -18,14 +18,16 @@ def gen_thumbnail(bytes_, resolution):
     max_x, max_y = resolution
     image = Image.open(bytes_io)
     current_x, current_y = image.size
-    fac_x = max_x // current_x
-    fac_y = max_y // current_y
+    fac_x = max_x / current_x
+    fac_y = max_y / current_y
 
     if fac_x >= 1 or fac_y >= 1:
         raise NoThumbnailRequired()
 
     factor = min(fac_x, fac_y)
-    thumbnail_size = (current_x * factor, current_y * factor)
+    new_x = min(max_x, round(current_x * factor))
+    new_y = min(max_y, round(current_y * factor))
+    thumbnail_size = (new_x, new_y)
     image.thumbnail(thumbnail_size, Image.ANTIALIAS)
 
     with NamedTemporaryFile('w+b', suffix='.jpg') as thumbnail:
