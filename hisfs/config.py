a74f4d2ff7fd7871fbc5f7d5b7d58c0300dd6bf9
Richard Neumann <r.neumann@homeinfo.de>
2018-12-07 15:21:54 +0100

    Refac.

--- hisfs/config.py
+++ hisfs/config.py
@@ -1,11 +1,12 @@
 """Configuration parser."""
 
-from configlib import loadcfg, JSONParser
+from configlib import load_ini, load_json
+
 
 __all__ = ['LOG_FORMAT', 'CONFIG', 'DEFAULT_QUOTA', 'HOOKS']
 
 
 LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
-CONFIG = loadcfg('hisfs.conf')
+CONFIG = load_ini('hisfs.conf')
 DEFAULT_QUOTA = int(CONFIG['fs']['quota'])
-HOOKS = JSONParser('/usr/local/etc/hisfs.hooks', alert=True)
+HOOKS = load_json('hisfs.hooks')
