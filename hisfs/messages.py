883af016559919bef18cb0b96646609ca984f3ee
Richard Neumann <r.neumann@homeinfo.de>
2019-03-14 15:05:48 +0100

    Refac.

--- hisfs/messages.py
+++ hisfs/messages.py
@@ -1,6 +1,6 @@
 """Errors of the FS."""
 
-from his import HIS_MESSAGE_FACILITY
+from wsgilib import JSONMessage
 
 __all__ = [
     'NO_SUCH_FILE',
@@ -13,15 +13,12 @@ __all__ = [
     'NOT_A_PDF_DOCUMENT']
 
 
-HISFS_MESSAGE_DOMAIN = HIS_MESSAGE_FACILITY.domain('hisfs')
-HISFS_MESSAGE = HISFS_MESSAGE_DOMAIN.message
-NO_SUCH_FILE = HISFS_MESSAGE('The requested file does not exist.', status=404)
-FILE_CREATED = HISFS_MESSAGE('The file has been created.', status=201)
-FILES_CREATED = HISFS_MESSAGE('The files have been created.', status=200)
-FILE_EXISTS = HISFS_MESSAGE('The file already exists.', status=409)
-FILE_DELETED = HISFS_MESSAGE('The file has been deleted.', status=200)
-READ_ERROR = HISFS_MESSAGE('Could not read file.', status=500)
-QUOTA_EXCEEDED = HISFS_MESSAGE(
+NO_SUCH_FILE = JSONMessage('The requested file does not exist.', status=404)
+FILE_CREATED = JSONMessage('The file has been created.', status=201)
+FILES_CREATED = JSONMessage('The files have been created.', status=200)
+FILE_EXISTS = JSONMessage('The file already exists.', status=409)
+FILE_DELETED = JSONMessage('The file has been deleted.', status=200)
+READ_ERROR = JSONMessage('Could not read file.', status=500)
+QUOTA_EXCEEDED = JSONMessage(
     'You have reached your disk space quota.', status=403)
-NOT_A_PDF_DOCUMENT = HISFS_MESSAGE(
-    'The file is not a PDF document.', status=400)
+NOT_A_PDF_DOCUMENT = JSONMessage('The file is not a PDF document.', status=400)
