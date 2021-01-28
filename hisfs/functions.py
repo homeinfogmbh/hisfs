5be9e2ee5c2dc676ddf3f55be9d004929bb118f6
Richard Neumann <mail@richard-neumann.de>
2021-01-28 12:18:18 +0100

    Limited file selection to current customer.

--- hisfs/functions.py
+++ hisfs/functions.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from peewee import ModelSelect
 
-from his import ACCOUNT, CUSTOMER
+from his import CUSTOMER
 
 from hisfs.messages import NO_SUCH_FILE
 from hisfs.orm import File
@@ -16,12 +16,8 @@ __all__ = ['get_file', 'get_files']
 def get_files(shallow: bool = True) -> ModelSelect:
     """Selects files."""
 
-    condition = True
-
-    if not ACCOUNT.root:
-        condition &= File.customer == CUSTOMER.id
-
-    return File.select(cascade=True, shallow=shallow).where(condition)
+    return File.select(cascade=True, shallow=shallow).where(
+        File.customer == CUSTOMER.id)
 
 
 def get_file(file_id: Union[int, File], *,
