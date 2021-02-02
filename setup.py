0b432e179aedfcf3302c3690fe769209d5640275
Richard Neumann <mail@richard-neumann.de>
2021-02-02 10:42:52 +0100

    Migrated to setuptools.

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='hisfs',
-    version='latest',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
+    install_requires=[
+        'PIL',
+        'configlib',
+        'filedb',
+        'flask',
+        'his',
+        'mdb',
+        'peewee',
+        'peeweeplus',
+        'wsgilib',
+    ],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo period de>',
-    requires=['his'],
+    maintainer_email='<r.neumann@homeinfo.de>',
     packages=['hisfs'],
-    description='HOMEINFO Integrated Services File System')
+    description='HOMEINFO Integrated Services File System'
+)
