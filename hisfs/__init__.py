cb21f4a414d072a566b381ca8a8406bc336e74fe
Richard Neumann <mail@richard-neumann.de>
2021-09-23 14:22:51 +0200

    Export ERRORS

--- hisfs/__init__.py
+++ hisfs/__init__.py
@@ -1,8 +1,9 @@
 """HIS module providing a file system."""
 
+from hisfs.errors import ERRORS
 from hisfs.exceptions import QuotaExceeded
 from hisfs.functions import get_file
 from hisfs.orm import File
 
 
-__all__ = ['QuotaExceeded', 'get_file', 'File']
+__all__ = ['ERRORS', 'QuotaExceeded', 'get_file', 'File']
