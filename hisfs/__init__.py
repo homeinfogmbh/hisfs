fa0136e31c6071311f9e8006cfc1fc7a92831cdb
Richard Neumann <r.neumann@homeinfo.de>
2022-04-08 11:34:29 +0200

    Export FileExists

--- hisfs/__init__.py
+++ hisfs/__init__.py
@@ -1,9 +1,9 @@
 """HIS module providing a file system."""
 
 from hisfs.errors import ERRORS
-from hisfs.exceptions import QuotaExceeded
+from hisfs.exceptions import FileExists, QuotaExceeded
 from hisfs.functions import get_file
 from hisfs.orm import File
 
 
-__all__ = ['ERRORS', 'QuotaExceeded', 'get_file', 'File']
+__all__ = ['ERRORS', 'FileExists', 'QuotaExceeded', 'get_file', 'File']
